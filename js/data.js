14c9bcf635ba2ad86629f44c19bbe99beba867e4
Md-Jaabir <100750999+Md-Jaabir@users.noreply.github.com>
2023-03-24 11:55:11 +0600

    initial commit

---
+++ js/data.js
@@ -0,0 +1,18 @@
+const articles = [
+  {
+    title: "Top 5 ways to use ChatGPT effectively",
+    shortDesc: "ChatGPT is an impressive AI chatbot created by OpenAI that can answer almost any question you throw...",
+    date: "23-03-2023",
+    category: "tech",
+    cover: "./assets/chatgpt.png",
+    articleUrl: "./articles/chatgpt.html"
+  },
+  {
+    title: "Pi!!!",
+    shortDesc: "Pi (π) is an irrational number and a Greek alphabet that represents the ratio of the circumference and diameter of a circle ...",
+    date: "22-03-2023",
+    category: "math",
+    cover: "./assets/pi-cover.png",
+    articleUrl: "./articles/pi.html"
+  }
+];
