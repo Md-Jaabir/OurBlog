58fc26c70db2ea950c6d87328590290d1e335615
Md-Jaabir <100750999+Md-Jaabir@users.noreply.github.com>
2023-03-31 14:27:33 +0600

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,50 @@
+const articles = [
+  {
+    title:"BDMO 2012 Regional problem with solution",
+    shortDesc: "Can you solve this problem???",
+    date: "31-03-2023",
+    category: "math",
+    cover: "./assets/mathproblemone.png",
+    articleUrl: "./articles/mathproblemone.html"
+    },
+    {
+    title:"Top 5 chess opening principles thar you must know",
+    shortDesc: "The opening stage of a chess game is critical because...",
+    date: "30-03-2023",
+    category: "chess",
+    cover: "./assets/chessopening.png",
+    articleUrl: "./articles/chessopeningprinciples.html"
+    },  
+    {
+    title: "Bangladesh acheived another t20 victory",
+    shortDesc: "Yesterday, the first T20 match between Bangladesh and Ireland was held at Jahur Ahmed Chowdhury...",
+    date: "28-03-2023",
+    category: "sports",
+    cover: "./assets/ban-ire-t20-one.png",
+    articleUrl: "./articles/banvsiret20one.html"
+   },  
+   {
+    title: "Bangladesh washed Ireland in the 3 match ODI seriese",
+    shortDesc: "Bangladesh achieved yet another clean sweep against Ireland in the recently concluded...",
+    date: "24-03-2023",
+    category: "sports",
+    cover: "./assets/ban-ire.png",
+    articleUrl: "./articles/banvsireodi.html"
+  },
+  {
+    title: "Top 5 ways to use ChatGPT effectively",
+    shortDesc: "ChatGPT is an impressive AI chatbot created by OpenAI that can answer almost any question you throw...",
+    date: "23-03-2023",
+    category: "tech",
+    cover: "./assets/chatgpt.png",
+    articleUrl: "./articles/chatgpt.html"
+  },
+  {
+    title: "Pi!!!",
+    shortDesc: "Pi (π) is an irrational number and a Greek alphabet that represents the ratio of the circumference and diameter of a circle ...",
+    date: "22-03-2023",
+    category: "math",
+    cover: "./assets/pi-cover.png",
+    articleUrl: "./articles/pi.html"
+  }
+];
---
+++ js/script.js
@@ -0,0 +1,40 @@
+window.onload=()=>{
+  document.querySelector(".hamburger i").addEventListener("click",toggleNav);
+  fetchRecent();
+}
+
+const navigate=(url)=>{
+  location.href=url;
+}
+
+
+const toggleNav=()=>{
+  if([...document.querySelector(".links").classList].includes("active")){
+    document.querySelector(".links").classList.remove('active');
+    document.querySelector(".hamburger i").className="fa-solid fa-bars";
+  }else{
+    document.querySelector(".links").classList.add('active');
+    document.querySelector(".hamburger i").className="fa-solid fa-xmark";
+  }
+}
+
+const fetchRecent=()=>{
+  const container=document.querySelector("main .recent .posts");
+  let markup="";
+  for(let i=0;i<4;i++){
+  let article=articles[i];
+    if(article){
+      markup+=`<div onclick="navigate('${article.articleUrl}')" class="post">
+          <img src="${article.cover}">
+          <div class="detail">
+            <h2>${article.title}</h2>
+            <p>${article.shortDesc}</p>
+            <h4>Date : ${article.date}</h4>
+          </div>
+        </div>`;
+    }
+  }
+  if(container){
+    container.innerHTML=markup;
+  }
+}
